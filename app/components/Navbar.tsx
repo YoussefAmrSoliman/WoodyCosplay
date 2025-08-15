41e6bc31a77fc9ccf6463d619e3e9daf4e7b81bb
Youssef <youssef.amr79@gmail.com>
2025-08-15 22:31:58 +0300

    fixing hamburger menu

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -8,16 +8,18 @@ const Navbar = () => {
 
     return (
         <nav className="flex flex-row items-center bg-gray-800/60 text-white p-4 
-        sticky top-0 z-10 rounded-b-4xl backdrop-blur-md">
+        sticky top-0 z-10 rounded-b-4xl backdrop-blur-md h-16">
             <div className="md:hidden absolute right-4">
                 <HamburgerButton isOpen={isOpen} setIsOpen={setIsOpen} />
             </div>
             <h1 className="text-lg font-bold">Woody Cosplay</h1>
-            
-            <div className={`md:hidden absolute top-full  w-40
+
+            <div className={`md:hidden fixed top-16 right-0 w-40
                 bg-white shadow-lg rounded-3xl mt-2 z-50 flex flex-col
-                ${isOpen ? '-right-[-0.5%] opacity-100': 'right-[-100%] opacity-0'} 
-                transition-all duration-300`}>
+                ${isOpen ? 'translate-x-0 opacity-100': 'translate-x-full opacity-0'} 
+                transition-all duration-300`}
+                aria-hidden={!isOpen}>
+                    
                 <button className="py-2 px-4
                     text-gray-900 hover:bg-gray-100
                     cursor-pointer rounded-3xl">Home</button>
